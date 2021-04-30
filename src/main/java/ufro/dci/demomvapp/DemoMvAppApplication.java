8defc03d8d1486ede88733e8d590f04ad02104e3
unknown <a.sandoval17@ufromail.cl>
2021-04-30 09:18:37 -0400

    proyecto base

---
+++ src/main/java/ufro/dci/demomvapp/DemoMvAppApplication.java
@@ -0,0 +1,13 @@
+package ufro.dci.demomvapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufro/dci/demomvapp/DemoMvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ufro.dci.demomvapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
